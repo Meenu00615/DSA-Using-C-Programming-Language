9dfa3527352aa02d3f8da6cb44439bb0939167db
Meenu Patel <149779716+Meenu00615@users.noreply.github.com>
2024-03-26 21:51:49 +0530

    Add files via upload

---
+++ Namaste Duniya.cpp
@@ -0,0 +1,8 @@
+#include<iostream>   //header file for the essential programming input and output
+using namespace std;   //contain library function objects
+
+int main(){
+	cout <<"Namaste Dunia By Meenu Patel"<<endl;  //display Namaste Duniya
+	cout <<"Namaste Dunia\n";      //without endl
+	cout<<"\n";   //for entering the new line in c++
+}
