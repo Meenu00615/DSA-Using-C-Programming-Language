468d2cc338c3fa7c86e80b307905eae0c8553414
Meenu Patel <149779716+Meenu00615@users.noreply.github.com>
2024-03-31 23:39:30 +0530

    Add files via upload

---
+++ Data Types in C++.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*	int a = 28;   //size : 4
+	int b = 34;
+	int sum = a+b;
+	cout<<sum<<endl;
+	char c = 'a';      //only single character
+	//size: 1 
+	cout<<c<<endl;
+	
+	bool bl = true;   //size: 2
+	cout<<bl<<endl;
+	
+	double dl = 4.55;  //size : 8
+	cout<<dl<<endl;
+	
+	float f= 3.3;      //size: 4
+	cout<<f<<endl;
+	
+	//Checking the size of the above different variables
+	int size = sizeof(sum);
+	cout<<"Size of a variable: "<<size<<endl;
+*/
+    int a = 'a';
+    cout<<a<<endl;
+    
+    float fl = 4.6542342234235;
+    int z = int(fl);
+    cout<<z<<endl;
+}
+
