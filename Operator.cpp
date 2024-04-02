4cdd08d157d036ea834eeedae5747fa0f7b8f2bc
Meenu Patel <149779716+Meenu00615@users.noreply.github.com>
2024-04-02 23:55:51 +0530

    Add files via upload

---
+++ Operator.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	unsigned int k = 23;
+	cout<<k<<endl;
+
+	double  f = 2.0/5;
+	cout<<f<<endl;
+	cout<<2.0/5<<endl;
+
+	int p=5;
+	int b = 8;
+
+	bool first = (p==b);
+	cout<<first<<endl;
+	bool second = (p>b);
+	cout<<second<<endl;
+	bool third = (p<b);
+	cout<<third<<endl;
+	bool four = (p>=b);
+	cout<<four<<endl;
+	bool five = (p<=b);
+	cout<<five<<endl;
+	bool six= (p!=b);
+	cout<<six<<endl;
+
+    int o=0;
+	cout<<!o<<endl;
+	int o1=1;
+	cout<<o1<<endl;
+}
+    
+    
+
+    
